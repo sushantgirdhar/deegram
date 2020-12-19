c5d628ce1daf85a0122d552f4e14d24acf1f4d93
SUSHANT GIRDHAR <52759137+sushantgirdhar@users.noreply.github.com>
2020-12-20 00:16:08 +0530

    Update translate.py

--- deegram/utils/translate.py
+++ deegram/utils/translate.py
@@ -1,6 +1,6 @@
 VERSION = "0.1.1"
-CREATOR = "@aykut"
-BOT_NAME = "Deegram"
+CREATOR = "@sushantgirdhar"
+BOT_NAME = "EBRUISER-MUSIC"
 WELCOME_MSG = (
 	f"🎶🎵 Welcome to {BOT_NAME} 🎵🎶\n\n"
 	"Click /help for more information."
